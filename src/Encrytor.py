e15f921edca0a392bc02f84d1b24f3ff6c93752f
FPSInteractive <eTmullall@outlook.com>
2025-11-12 15:22:16 -0600

    Implement key storage and modify delete_key method
    Added key saving functionality and updated delete method.

--- src/Encrytor.py
+++ src/Encrytor.py
@@ -1,13 +1,18 @@
 from cryptography.fernet import Fernet
 import os 
-    
+from fs import FileSystem
+
 class Key:
     def __init__(self):
         self.key = Fernet.generate_key()
-        
+        if not os.path.exists("key.key"):
+            self.save_key()
+                
     def delete_key(self, key, FileSystem):
         if key != self.key:
             print("Might as well say goodbye to your files and directories now!")
-            FileSystem.delete()
-
-
+            FileSystem.delete_key()
+            
+    def save_key(self):
+        with open("key.key", "w") as key:
+            key.write(self.key)
